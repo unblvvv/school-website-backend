d8c0fce4328972a9c6ed1305b5b0fd9b41141814
Nikita Kapinos <nekitcom111@gmail.com>
2023-04-17 09:52:07 +0300

    first commit

---
+++ src/main/java/ua/unblv/schoolwebsite/SchoolWebsiteApplication.java
@@ -0,0 +1,13 @@
+package ua.unblv.schoolwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolWebsiteApplication.class, args);
+	}
+
+}
